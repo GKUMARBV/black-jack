f34a3929f41c47de265459f3819a89bf8653b607
Gowtham Kumar B V <gkumarbv@deloitte.com>
2024-01-14 14:10:09 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'blackJack',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
